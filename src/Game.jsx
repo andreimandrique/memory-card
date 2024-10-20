37a04353390a69f52df554e2e5ef37629539ff5f
andrei <andreimandrique3@gmail.com>
2024-10-20 21:58:46 +0800

    fix random

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,14 +1,25 @@
 import { useState, useEffect } from "react";
 import Card from "./Card";
 
+function shuffle(array) {
+  const copy = [...array];
+
+  for (let i = array.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    [array[i], array[random]] = [array[random], array[i]];
+  }
+
+  return copy;
+}
+
 function Game() {
   const [data, setData] = useState([]);
 
+  const API = "https://rickandmortyapi.com/api/character/[1,2,3]";
+
   async function getData() {
     try {
-      const response = await fetch(
-        "https://rickandmortyapi.com/api/character/[1,2,3]"
-      );
+      const response = await fetch(API);
       const data = await response.json();
       setData(data);
     } catch (error) {
@@ -20,13 +31,22 @@ function Game() {
     getData();
   }, []);
 
+  function handleClick() {
+    setData((prevData) => shuffle(prevData));
+  }
+
   const cardList = data.map((item) => (
     <div key={item.id}>
       <Card title={item.name} imgUrl={item.image}></Card>
     </div>
   ));
 
-  return <div>{cardList}</div>;
+  return (
+    <>
+      <button onClick={handleClick}>Random</button>
+      <div>{cardList}</div>
+    </>
+  );
 }
 
 export default Game;
